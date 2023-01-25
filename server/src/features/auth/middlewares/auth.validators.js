8f31b947499e8b331e1966815cefebeb23e82949
samirjabib <samirdev9669@gmail.com>
2023-01-25 10:59:07 -0500

    add joi validators to auth

--- server/src/features/auth/middlewares/auth.validators.js
+++ server/src/features/auth/middlewares/auth.validators.js
@@ -1,6 +1,6 @@
 const Joi = require('joi')
 
-const userShema = Joi.object({
+const registerSchema = Joi.object({
     displayName:Joi.string()
         .min(3)
         .max(40)
@@ -18,3 +18,39 @@ const userShema = Joi.object({
         .optional()
 })
 
+const loginSchema = Joi.object({
+    email:Joi.string()
+        .email()
+        .min(3)
+        .max(30),
+    password:Joi.string()
+        .min(3)
+        .max(50)
+        .required()
+})
+
+const validateRegister = ( req, res, next ) => {
+    const data = req.body
+
+    const { error } = registerSchema.validate(data, { abortEarly: false } )
+
+    if(error){
+        return res.json({ error: error.details })
+    }
+
+    next()
+}
+
+const validateLogin = (req, res, next) => {
+
+    const data = req.body
+    const { error } = loginSchema.validate( data, { abortEarly:false })
+
+    if(error){
+        return res.json( { error: error.details })
+    }
+
+    next()
+}
+
+module.exports = { validateRegister, validateLogin }
