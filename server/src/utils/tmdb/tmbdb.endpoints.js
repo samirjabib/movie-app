86da1165ad4ccd9bf28b0c7b8fc67031b168cb41
samirjabib <samirdev9669@gmail.com>
2023-01-26 10:59:58 -0500

    add

--- server/src/utils/tmdb/tmbdb.endpoints.js
+++ server/src/utils/tmdb/tmbdb.endpoints.js
@@ -1,8 +1,35 @@
-const tmbdbConfig = require('./tmdb.config')
+const tmdbConfig = require('./tmdb.config')
 
 const tmdbEndPoints = {
     mediaList : ({ mediaType, mediaCategory, page}) => tmbdbConfig.getUrl(
         `${mediaType}/${mediaCategory}`, {page}
+    ),
+    mediaDetail:({mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}`
+    ),
+    mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(
+        `genre/${mediaType}/list`
+    ),
+    mediaCredits:({ mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/credits`
+    ),
+    mediaVideos: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/videos`
+    ),
+    mediaRecommend: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+    `${mediaType}/${mediaId}/recommendations`
+    ),
+    mediaImages: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/images`
+    ),
+    mediaSearch: ({ mediaType, query, page }) => tmdbConfig.getUrl(
+        `search/${mediaType}`, { query, page }
+    ),
+    personDetail: ({ personId }) => tmdbConfig.getUrl(
+        `person/${personId}`
+    ),
+    personMedias: ({ personId }) => tmdbConfig.getUrl(
+        `person/${personId}/combined_credits`
     ),
     mediaSearch: ({ mediaType, query, page}) => tmbdbConfig.getUrl(
         `search/${mediaType}`, { query, page }
