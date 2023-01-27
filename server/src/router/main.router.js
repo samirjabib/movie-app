4b76a36ac9fcd5cb01e4620cdd9759cdc932a34a
samirjabib <samirdev9669@gmail.com>
2023-01-27 14:30:32 -0500

    add

--- server/src/router/main.router.js
+++ server/src/router/main.router.js
@@ -1,5 +1,6 @@
 const { authRouter } = require("../features/auth/auth.routes")
 const { mediaRouter } = require("../features/media/media.routes")
+const { personRouter } = require("../features/person/person.routes")
 
 
 
@@ -7,6 +8,6 @@ const mainRouter = require("express").Router()
 
 mainRouter.use('/auth', authRouter)
 mainRouter.use('/media', mediaRouter)
-
+mainRouter.use('/person', personRouter)
 
 module.exports = { mainRouter }
