ebf4fc18e1e985e383a1d97ff81dfae9abd68c4e
samirjabib <samirdev9669@gmail.com>
2023-01-27 14:42:22 -0500

    add

---
+++ server/src/features/favorites/favorite.controller.js
@@ -0,0 +1,37 @@
+const { Favorites } = require('../../models/sql/favorite.model')
+
+
+
+
+const addFavorite = async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+const removeFavorite = async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+const getFavoritesOfUser = async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+module.exports = {
+    addFavorite,
+    removeFavorite,
+    getFavoritesOfUser
+}
