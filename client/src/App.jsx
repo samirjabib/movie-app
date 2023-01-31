7f9acb8b8c82537431c39def9c0e633a761c8067
samirjabib <samirdev9669@gmail.com>
2023-01-31 16:03:09 -0500

    add custom hook for themeProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { useProviderTheme } from "./hooks"
 
 function App() {
 
+  const {  handleThemeSwitch  } = useProviderTheme()
+
   return (
-    <div className="App flex items-center justify-center h-screen w-full">
-      Im work
+    <div className="h-screen w-full flex justify-center items-center gap-4">
+      <span className="w-40 bg-black text-white h-40 flex items-center justify-center dark:bg-gray-200 dark:text-black ">Hola</span>
+      <span className="w-40 bg-gray-200 text-black  dark:text-white h-40 flex items-center justify-center dark:bg-black">Hola</span>
+      <button className="py-4 px-12 bg-red-500 absolute mx-auto top-52 botton-0 left-0 right-0 w-32 text-center"
+            onClick={handleThemeSwitch}
+        >Change DARK</button>
     </div>
   )
 }
---
+++ client/src/hooks/useProviderTheme.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export const useProviderTheme = () => {
+
+    const [ theme, setTheme ] = useState(null)
+
+
+    useEffect( () => {
+        if(window.matchMedia('prefers-colo-sheme: dark').matches){
+            setTheme('dark')
+        }
+        setTheme('light')
+    }, [])
+    
+    useEffect(() => {
+        if (theme === "dark") {
+        document.documentElement.classList.add("dark");
+        } else {
+        document.documentElement.classList.remove("dark");
+        }
+      }, [theme]);
+    
+    
+    const handleThemeSwitch = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+    
+    
+    
+    return{
+        handleThemeSwitch
+    }
+}
+
