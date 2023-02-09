610b2cd551323c42ddc4cc5148c68be4659bf5b0
samirjabib <samirdev9669@gmail.com>
2023-02-09 15:37:49 -0500

    add

--- client/src/features/Home/components/HeroSlide.jsx
+++ client/src/features/Home/components/HeroSlide.jsx
@@ -38,13 +38,14 @@ export const HeroSlide = ({ mediaType, mediaCategory}) => {
             <Swiper
                 grabCursor={true}
                 loop={true}
+                preventInteractionOnTransition={true}
             >
                 {
                     popularMovies?.map( (movie, index) => {
                         return(
                             <SwiperSlide key={index}>
                                 
-                                <div className="w-full bg-white dark:bg-black border-5 border-yellow-500 ">
+                                <div className="w-full bg-white dark:bg-black  ">
                                     <div className="w-full h-screen bg-gradient-to-r absolute from-green-100 dark:from-black"></div>
                                     <img
                                         src={tmdbConfigs.backdropPath(movie.backdrop_path || movie.poster_path)}
@@ -57,23 +58,32 @@ export const HeroSlide = ({ mediaType, mediaCategory}) => {
                                             {movie.title}
                                         </h2>
                                         <div 
-                                            className="w-96  flex flex-row-reverse mt-2"
+                                            className="w-96 flex flex-row mt-2 justify-between"
                                         >
-                                    
-                                                {[...movie.genre_ids].splice(0,3).map(
-                                                    (genreId, index )=> {
-                                                        return(
-                                                            <div 
-                                                            className="text-gray-700 dark:text-green-200 full ml-2"
-                                                            key={index}
-                                                        >
-                                                            {arrayCategories?.find( categorie => categorie.id === genreId ) && arrayCategories?.find( categorie => categorie.id === genreId).name}
-                                                            </div>
-                                                        )
-                                                       
+                                                <div class="flex flex-row self-center">
+                                                        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                                                        <p class="ml-2 text-sm font-bold text-gray-500 dark:text-green-200">                                                    
+                                                            {movie.vote_average}
+                                                        </p>
+                                
+                                                </div>
+                                                <div className="flex flex-row">
+                                                    {[...movie.genre_ids].splice(0,2).map(
+                                                        (genreId, index )=> {
+                                                            return(
+                                                                <div 
+                                                                className="text-gray-700 dark:text-green-200 full ml-2"
+                                                                key={index}
+                                                            >
+                                                                {arrayCategories?.find( categorie => categorie.id === genreId ) && arrayCategories?.find( categorie => categorie.id === genreId).name}
+                                                                </div>
+                                                            )
+                                                        
 
-                                                    }
-                                                )}
+                                                        }
+                                                    )}
+                                                </div>
+
                                         </div>
                                         <p className="text-black dark:text-white mt-6 w-96">
                                             {reducedString(movie.overview, 250)}
