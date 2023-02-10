cd0caeb6e707f8e22f3bc2780d4d4172402e2fd2
samirjabib <samirdev9669@gmail.com>
2023-02-10 08:46:38 -0500

    status

--- client/src/features/Home/pages/HomePage.jsx
+++ client/src/features/Home/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { tmdbConfigs } from "../../../api";
 
 
 
+
 export const HomePage = () => {
     return (
         <div className=" h-auto bg-white dark:bg-black ">
@@ -12,24 +13,24 @@ export const HomePage = () => {
                 mediaCategory={tmdbConfigs.mediaCategory.popular}
             />
             <MediaSlide
-                title="popular movies"
+                title="top rated movies"
                 mediaType={tmdbConfigs.mediaType.movie}
                 mediaCategory={tmdbConfigs.mediaCategory.popular}
             />
             <MediaSlide
                 title="popular series"
                 mediaType={tmdbConfigs.mediaType.movie}
-                mediaCategory={tmdbConfigs.mediaCategory.popular}
+                mediaCategory={tmdbConfigs.mediaCategory.top_rated}
             />
             <MediaSlide
                 title="top rates movies"
-                mediaType={tmdbConfigs.mediaType.movie}
+                mediaType={tmdbConfigs.mediaType.tv}
                 mediaCategory={tmdbConfigs.mediaCategory.popular}
             />
             <MediaSlide
                 title="top rated series"
-                mediaType={tmdbConfigs.mediaType.movie}
-                mediaCategory={tmdbConfigs.mediaCategory.popular}
+                mediaType={tmdbConfigs.mediaType.tv}
+                mediaCategory={tmdbConfigs.mediaCategory.top_rated}
             />
         </div>
     );
