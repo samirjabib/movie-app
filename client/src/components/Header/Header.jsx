33a4930f6e1d69d952c0dd8a05d1208cf8f03c4e
samirjabib <samirdev9669@gmail.com>
2023-02-13 16:44:45 -0500

    add

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,8 +10,6 @@ export const Header = ({auth}) => {
     const { theme, handleThemeSwitch } = useProviderTheme()
     const { backgroundScroll, backgroundHandle } = useBackgroundScroll()
 
-
-
     return (
         <nav 
             className={`flex z-50 justify-between p-4 fixed top-0 w-full ${backgroundScroll ? 'bg-transparent' : 'bg-white dark:bg-[#131313]'}`}
@@ -19,19 +17,19 @@ export const Header = ({auth}) => {
         >
             <div className="flex gap-8">
                 <h1 className="font-bold text-2xl self-center  dark:text-white">Oni<span className="text-green-500">Giri</span></h1>
-                    <ul className="flex gap-4 self-center">
+                    <ul className="gap-4 self-center hidden md:flex">
                         <NavLinks navLinks={navLinks}/>
-                    </ul>
-                    <button 
-                        className="self-center duration-100 transition-all rounded-full p-2 delay-100 hover:bg-green-500 "
+                        <button 
+                        className="duration-100 transition-all rounded-full delay-100 hover:bg-green-500 relative bottom-[1px]"
                         onClick={handleThemeSwitch}
-                        
-                    >
-                        <MdOutlineDarkMode 
-                            size={24}
-                            color={`${ (theme === 'light') ? '#000000' : '#fff'}`}
-                        />
-                    </button>
+                        >
+                            <MdOutlineDarkMode 
+                                size={24}
+                                color={`${ (theme === 'light') ? '#000000' : '#fff'}`}
+                            />
+                        </button>
+                    </ul>
+                   
             </div>
             <button 
                 className="bg-green-500 text-sm shadow uppercase rounded-md font-medium dark:text-white px-4 py-2"
